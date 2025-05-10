1440f1dbe677fdac9f52c76d025df0d694720aef
Yuriportf <ysoli7777@gmail.com>
2025-05-10 10:26:02 -0300

    melhorias visuais e organizacionais

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ function copiarTexto(texto) {
       alert("Copiado: " + texto);
     });
   }
+
+  function toggleDoacoes() {
+    const secao = document.getElementById("secaoDoacoes");
+    secao.style.display = secao.style.display === "none" ? "block" : "none";
+  }
+
+  const frases = [
+    "Onde há partilha, há esperança.",
+    "Servir com dignidade é acolher com amor.",
+    "Juntos, transformamos vidas.",
+    "Alimente o corpo. Fortaleça o espírito.",
+    "Pequenos gestos, grandes milagres."
+  ];
+
+  let index = 0;
+  const fraseElemento = document.getElementById("fraseRotativa");
+
+  setInterval(() => {
+    index = (index + 1) % frases.length;
+    fraseElemento.textContent = frases[index];
+  }, 5000);
